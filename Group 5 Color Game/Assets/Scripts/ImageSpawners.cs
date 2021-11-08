79c04b818fb2170ea9dc97a2049936cb14e4ddfe
Christian <cgulick@student.fitchburgstate.edu>
2021-11-07 23:52:21 -0500

    Scuff Spawning

---
+++ Group 5 Color Game/Assets/Scripts/ImageSpawners.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageSpawners : MonoBehaviour
+{
+    public List<GameObject>  images;
+    private GameObject imagesP;
+    public bool iZero,iOne,iTwo,iThree,iFour,iFive,iSix;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpriteSpawn(Vector3 pos)
+    {
+        
+            
+                int randomImages = Random.Range(0,images.Count);
+                imagesP = images[randomImages];
+                
+                switch (randomImages)
+                {
+                    case 0:
+                    if (iZero == false)
+                    {
+                        Instantiate(images[0],pos, Quaternion.identity);
+                        //images.RemoveAt(0);
+                        //++p;
+  
+                    }
+                        break;
+                    case 1:
+                    if (iOne == false)
+                    {
+                        Instantiate(images[1],pos, Quaternion.identity);
+                        //images.RemoveAt(1);
+                        iOne = true;
+                        
+                        //++p;
+                    }
+                        break;
+                    case 2:
+                    if (iTwo == false)
+                    {
+                        Instantiate(images[2],pos, Quaternion.identity);
+                        //images.RemoveAt(2);
+                        iTwo = true;
+                        
+                        //++p;
+                    }
+                        break;
+                    case 3:
+                    if (iThree == false)
+                    {
+                        Instantiate(images[3],pos, Quaternion.identity);
+                        //images.RemoveAt(3);
+                        iThree = true;
+                        
+                        //++p;
+                    }
+                        break;
+                    case 4:
+                    if(iFour == false)
+                    {
+                        Instantiate(images[4],pos, Quaternion.identity);
+                        //images.RemoveAt(4);
+                        iThree = true;
+                        
+                        //++p;
+                    }
+                        break;
+                    case 5:
+                    if (iFive == false)
+                    {
+                        Instantiate(images[5],pos, Quaternion.identity);
+                        //images.RemoveAt(5);
+                        iFive = true;
+                        
+                        //++p;
+                    }
+                        break;
+                   
+                }
+
+            
+    }
+}
+
+
--- Group 5 Color Game/Assets/Scripts/SpriteSpawners.cs
+++ Group 5 Color Game/Assets/Scripts/SpriteSpawners.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class SpriteSpawners : MonoBehaviour
 {
-    //public GameObject [] spawnPoints, images;
-    public List<GameObject> spawnPoints, images;
-    public int maxSpawnPoints, selectedSp, i, selectedImages, randomSpawnPoints;
+    public GameObject [] spawnPoints;
+    
+    public List<int> numbers;
+    public int maxSpawnPoints, selectedSp, i, selectedImages, randomSpawnPoints, p;
+    public bool cZero,cOne,cTwo,cThree,cFour,cFive;
+    private GameObject sP, imagesP;
+    private ImageSpawners imageSpawnersScript;
     // Start is called before the first frame update
     void Start()
     {
        SpawnPointGeneration();
+       imageSpawnersScript = FindObjectOfType<ImageSpawners>();
+
     }
 
     // Update is called once per frame
@@ -23,69 +29,192 @@ public class SpriteSpawners : MonoBehaviour
     {
         maxSpawnPoints = 4; // Max number of current spawn points.
         
-        for (i = 0; i < 2; i++) // Chooses spawn points till it hits the max amount of spawns.
+        while (i < maxSpawnPoints) // Chooses spawn points till it hits the max amount of spawns.
         {
-            randomSpawnPoints  = Random.Range(0,spawnPoints.Count); // Selects one of possible spawn points.
-            spawnPoints[selectedSp] = spawnPoints[randomSpawnPoints];  
-            //selectedSp = 4;                                                  // Checks what spawn points are selected
-            switch (selectedSp) // If spawn point is selected it is then activated.
+            //selectedSp = 0;
+            randomSpawnPoints  = Random.Range(0,spawnPoints.Length); // Randomizes which one gets picks
+            sP = spawnPoints[randomSpawnPoints];  // Selects one of possible spawn points.
+
+            switch (randomSpawnPoints) // If spawn point is selected it is then activated.
             {
                 case 0:
-                    spawnPoints[0].SetActive(true);
-                    spawnPoints.Remove(spawnPoints[0]);
+                    if (cZero == false)
+                    {
+                        Vector3 cZeroPos;
+                        
+                        cZeroPos = spawnPoints[0].transform.position;
+                        spawnPoints[0].SetActive(true);
+                        cZero = true;
+                        //imageSpawnersScript.SpriteSpawn(cZeroPos);
+
+                        //SpriteSpawn(cZeroPos);
+
+                        ++i;
+                    }
                     break;
+                    
                  case 1:
-                    spawnPoints[1].SetActive(true);
-                    spawnPoints.Remove(spawnPoints[1]);
+                     if (cOne == false)
+                    {
+                        Vector3 cOnePos;
+                        cOnePos = spawnPoints[1].transform.position;
+                        spawnPoints[1].SetActive(true);
+                        cOne = true;
+                        //imageSpawnersScript.SpriteSpawn(cOnePos);
+
+                        //SpriteSpawn(cOnePos);
+
+                        ++i;
+                    }
+
                     break;
                  case 2:
-                    spawnPoints[2].SetActive(true);
-                    spawnPoints.Remove(spawnPoints[2]);
+                     if (cTwo == false)
+                    {
+                        Vector3 cTwoPos;
+                        
+                        cTwoPos = spawnPoints[2].transform.position;
+                        spawnPoints[2].SetActive(true);
+                        cTwo = true;
+                        //imageSpawnersScript.SpriteSpawn(cTwoPos);
+
+                        //SpriteSpawn(cTwoPos);
+  
+                        ++i;
+                    }
+                    
                     break;
                  case 3:
-                    spawnPoints[3].SetActive(true);
-                    spawnPoints.Remove(spawnPoints[3]);
+                     if (cThree == false)
+                    {
+                        Vector3 cThreePos;
+                        
+                        cThreePos = spawnPoints[3].transform.position;
+                        spawnPoints[3].SetActive(true);
+                        cThree = true;
+                        //imageSpawnersScript.SpriteSpawn(cThreePos);
+                        
+                        //SpriteSpawn(cThreePos);
+
+                        ++i;
+                    }
+                     
                     break;
                  case 4:
-                    spawnPoints[4].SetActive(true);
-                    spawnPoints.Remove(spawnPoints[4]);
+                     if (cFour == false)
+                    {
+                        Vector3 cFourPos;
+                        
+                        cFourPos = spawnPoints[4].transform.position;
+                        spawnPoints[4].SetActive(true);
+                        cFour = true;
+                        //imageSpawnersScript.SpriteSpawn(cFourPos);
+
+                        //SpriteSpawn(cFourPos);
+
+                        ++i;
+                    }
+                     
                     break;
                  case 5:
-                    spawnPoints[5].SetActive(true);
-                    spawnPoints.Remove(spawnPoints[5]);
+                     if (cFive == false)
+                    {
+                        Vector3 cFivePos;
+                        cFivePos = spawnPoints[5].transform.position;
+                        spawnPoints[5].SetActive(true);
+                        //imageSpawnersScript.SpriteSpawn(cFivePos);
+                        cFive = true;
+
+                        //SpriteSpawn(cFivePos);
+
+                        ++i;
+                    }
                     break;
-                 
+                  
             }
+            
+            
+            
+            Debug.Log(i);
+            //Debug.Log(randomSpawnPoints);
         }
         
     }
+}
 
-    public void SpriteSpawn()
+    /*public void SpriteSpawn(Vector3 pos)
     {
-        int p;
-        for (p = 0; p < maxSpawnPoints; p++)
-            {
+        
+            
                 int randomImages = Random.Range(0,images.Count);
-                images[selectedImages] = images[selectedImages];
-                switch (selectedImages)
+                imagesP = images[randomImages];
+                
+                switch (randomImages)
                 {
                     case 0:
-                    Instantiate(images[1],spawnPoints[selectedSp].transform.position, spawnPoints[selectedSp].transform.rotation);
+                    if (iZero == false)
+                    {
+                        Instantiate(images[0],pos, Quaternion.identity);
+                        //images.RemoveAt(0);
+                        //++p;
+                        
+                        
+                        
+                    }
                         break;
                     case 1:
+                    if (iOne == false)
+                    {
+                        Instantiate(images[1],pos, Quaternion.identity);
+                        //images.RemoveAt(1);
+                        iOne = true;
+                        
+                        //++p;
+                    }
                         break;
                     case 2:
+                    if (iTwo == false)
+                    {
+                        Instantiate(images[2],pos, Quaternion.identity);
+                        //images.RemoveAt(2);
+                        iTwo = true;
+                        
+                        //++p;
+                    }
                         break;
                     case 3:
+                    if (iThree == false)
+                    {
+                        Instantiate(images[3],pos, Quaternion.identity);
+                        //images.RemoveAt(3);
+                        iThree = true;
+                        
+                        //++p;
+                    }
                         break;
                     case 4:
+                    if(iFour == false)
+                    {
+                        Instantiate(images[4],pos, Quaternion.identity);
+                        //images.RemoveAt(4);
+                        iThree = true;
+                        
+                        //++p;
+                    }
                         break;
                     case 5:
+                    if (iFive == false)
+                    {
+                        Instantiate(images[5],pos, Quaternion.identity);
+                        //images.RemoveAt(5);
+                        iFive = true;
+                        
+                        //++p;
+                    }
                         break;
-                    case 6:
-                        break;
+                   
                 }
 
-            }
+            
     }
-}
+}*/
