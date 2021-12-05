cb6a24bd88a6b92fa6cb8c354d11fc0d98b7ccb4
Christian <cgulick@student.fitchburgstate.edu>
2021-12-05 14:51:59 -0500

    Worked on Clock Button

---
+++ Group 5 Color Game/Assets/Scripts/ClockTool.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockTool : MonoBehaviour
+{
+    public GameObject pastTime, futureTime, pastClock, futureClock;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pastTimeLine();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void pastTimeLine ()
+    {
+        pastTime.SetActive(true);
+        futureTime.SetActive(false);
+        pastClock.SetActive(false);
+        futureTime.SetActive(true);
+    }
+
+    public void futureTimeLine()
+    {
+        pastTime.SetActive(false);
+        futureTime.SetActive(true);
+        pastClock.SetActive(true);
+        futureTime.SetActive(false);
+    }
+
+}
