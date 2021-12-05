a6e2d6fea4066dde24bde6e7c549ac6d7fb11347
Christian <cgulick@student.fitchburgstate.edu>
2021-12-04 22:04:42 -0500

    Added ToolBar Images

--- Group 5 Color Game/Assets/Scripts/InventoryBar.cs
+++ Group 5 Color Game/Assets/Scripts/InventoryBar.cs
@@ -5,17 +5,33 @@ using UnityEngine;
 public class InventoryBar : MonoBehaviour
 {
     
-    public GameObject inventoryBar;
+    public GameObject inventoryBar, toolOne, toolTwo, toolThree, toolFour, onButton, offButton;
     public GameObject [] toolImages;
 
     void Start()
     {
-        
+        toolBarOff();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Off Y-Height: -10.9
+        // ON Y-Height: .4
+    }
+    
+    private void toolBarOn()
+    {
+        onButton.SetActive(false);
+        offButton.SetActive(true);
+        inventoryBar.transform.position = new Vector3(0,.4f,0);
+
+    }
+
+    private void toolBarOff()
+    {
+        offButton.SetActive(false);
+        onButton.SetActive(true);
+        inventoryBar.transform.position = new Vector3(0,-10.9f,0);
     }
 }
